ebce1688ef50f2cac23ee76636ae7cbe429ff68c
mynameatgithub <tfanchia@mail.ccsf.edu>
2013-08-20 17:27:58 -0700

    Create ColorFactory.java

---
+++ ColorFactory.java
@@ -0,0 +1,15 @@
+/*
+ Author: Teresa Fanchiang
+ Date  : 04/10/13
+ Program Name: ColorFactory.java
+ Objective: This program allows
+ the user to make any color he wants.
+*/
+
+public class ColorFactory
+{
+    public static void main(String args[])
+    {
+        new BuildColorFactory();
+    }
+}
